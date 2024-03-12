925fe330314af1fb76a53520fbaf19b7d9a65f66
tagtaek <codetaek@gmail.com>
2024-03-12 19:21:24 +0900

    import&&export

---
+++ class/section01/01-08-welcome-template-api-import/index.js
@@ -0,0 +1,22 @@
+import { emailCheck, makingTemplate, sendTemplate } from "./email.js";
+
+function createUser({ name, age, school, email }) {
+  // 1. 이메일이 정상인지 확인(1-존재여부, 2-"@"포함여부)
+  const isVaild = emailCheck(email);
+  if (isVaild === false) return;
+
+  // 2. 가입환영 템플릿 만들기
+  const mytemplate = makingTemplate({ name, age, school, email });
+
+  // 3. 이메일에 가입환영 템플릿 전송하기
+  sendTemplate(email, mytemplate);
+}
+
+const name = "은하";
+const age = 23;
+const school = "비비지초등학교";
+const email = "galaxy@gmail.com";
+// const createdAt = getToday();
+
+createUser({ name, age, school, email });
+//8분 24초
---
+++ class/section01/01-08-welcome-template-api-import/utils.js
@@ -0,0 +1,3 @@
+export function getToday() {
+  return "2024-03-12"; // 실제로는 new Date() 사용
+}
