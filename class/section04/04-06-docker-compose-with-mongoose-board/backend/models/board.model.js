041b9858c44d32bfe0947149a713a312b2cf6203
tagtaek <codetaek@gmail.com>
2024-05-06 17:41:14 +0900

    04-06-docker-compose-with-mongoose-boards

---
+++ class/section04/04-06-docker-compose-with-mongoose-board/backend/models/board.model.js
@@ -0,0 +1,10 @@
+//board의 document schema(model)
+import mongoose from 'mongoose'
+
+const boardSchema = new mongoose.Schema({
+    writer: String,
+    title: String,
+    contents: String
+})
+
+export const Board = mongoose.model("Board", boardSchema)
